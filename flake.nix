115b54056767f637e025c1f693fc1bedc8f966f3
SPRAGE <shauna.pai@gmail.com>
2025-04-17 09:05:05 +0530

    Feat: Futures Data queries

--- flake.nix
+++ flake.nix
@@ -2,84 +2,97 @@
   description = "Rust with OpenSSL and Musl using Nix Flakes";
 
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
     naersk = {
-      url = github:nmattia/naersk;
+      url = "github:nmattia/naersk";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     fenix = {
-      url = github:nix-community/fenix;
+      url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    flake-utils.url = github:numtide/flake-utils;
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, naersk, fenix, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
+  outputs =
+    {
+      self,
+      nixpkgs,
+      naersk,
+      fenix,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
 
-      # Define Rust toolchain with musl target
-      toolchain = with fenix.packages.${system};
-        combine [
-          minimal.rustc
-          minimal.cargo
-          targets.x86_64-unknown-linux-musl.latest.rust-std
-        ];
+        # Define Rust toolchain with musl target
+        toolchain =
+          with fenix.packages.${system};
+          combine [
+            minimal.rustc
+            minimal.cargo
+            targets.x86_64-unknown-linux-musl.latest.rust-std
+          ];
 
-      # Use naersk with the custom Rust toolchain
-      naersk-lib = naersk.lib.${system}.override {
-        cargo = toolchain;
-        rustc = toolchain;
-      };
+        # Use naersk with the custom Rust toolchain
+        naersk-lib = naersk.lib.${system}.override {
+          cargo = toolchain;
+          rustc = toolchain;
+        };
 
-      # OpenSSL with Musl support
-      openssl = pkgs.openssl;
-      opensslStatic = pkgs.pkgsStatic.openssl;
-    in rec {
-      # Default package (builds Rust project with musl)
-      defaultPackage = packages.x86_64-unknown-linux-musl;
+        # OpenSSL with Musl support
+        openssl = pkgs.openssl;
+        opensslStatic = pkgs.pkgsStatic.openssl;
+      in
+      rec {
+        # Default package (builds Rust project with musl)
+        defaultPackage = packages.x86_64-unknown-linux-musl;
 
-      packages.x86_64-unknown-linux-musl = naersk-lib.buildPackage {
-        src = ./.;
+        packages.x86_64-unknown-linux-musl = naersk-lib.buildPackage {
+          src = ./.;
 
-        nativeBuildInputs = with pkgs; [
-          pkgsStatic.stdenv.cc  # Required for musl builds
-          openssl.dev           # OpenSSL headers
-          pkg-config            # Helps Rust find OpenSSL
-        ];
+          nativeBuildInputs = with pkgs; [
+            pkgsStatic.stdenv.cc # Required for musl builds
+            openssl.dev # OpenSSL headers
+            pkg-config # Helps Rust find OpenSSL
+          ];
 
-        buildInputs = [ opensslStatic ];
+          buildInputs = [ opensslStatic ];
 
-        # Environment variables to help Rust find OpenSSL
-        CARGO_BUILD_TARGET = "x86_64-unknown-linux-musl";
-        CARGO_BUILD_RUSTFLAGS = "-C target-feature=+crt-static";
-        OPENSSL_DIR = opensslStatic;
-        OPENSSL_LIB_DIR = "${opensslStatic.out}/lib";
-        OPENSSL_INCLUDE_DIR = "${opensslStatic.dev}/include";
+          # Environment variables to help Rust find OpenSSL
+          CARGO_BUILD_TARGET = "x86_64-unknown-linux-musl";
+          CARGO_BUILD_RUSTFLAGS = "-C target-feature=+crt-static";
+          OPENSSL_DIR = opensslStatic;
+          OPENSSL_LIB_DIR = "${opensslStatic.out}/lib";
+          OPENSSL_INCLUDE_DIR = "${opensslStatic.dev}/include";
 
-        # Ensure pkg-config can locate OpenSSL
-        PKG_CONFIG_PATH = "${opensslStatic.dev}/lib/pkgconfig";
+          # Ensure pkg-config can locate OpenSSL
+          PKG_CONFIG_PATH = "${opensslStatic.dev}/lib/pkgconfig";
 
-        doCheck = true;
-      };
+          doCheck = true;
+        };
 
-      # DevShell for interactive development
-      devShells.default = pkgs.mkShell {
-        packages = with pkgs; [
-          openssl
-        ];
+        # DevShell for interactive development
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            openssl
+            cargo
+            rustc
+          ];
 
-        shellHook = ''
-          export OPENSSL_DIR="${opensslStatic}"
-          export OPENSSL_LIB_DIR="${opensslStatic.out}/lib"
-          export OPENSSL_INCLUDE_DIR="${opensslStatic.dev}/include"
-          export PKG_CONFIG_PATH="${opensslStatic.dev}/lib/pkgconfig"
-          export PS1="(rust-musl-openssl) $PS1"
-        '';
-      };
-    });
+          shellHook = ''
+            export OPENSSL_DIR="${opensslStatic}"
+            export OPENSSL_LIB_DIR="${opensslStatic.out}/lib"
+            export OPENSSL_INCLUDE_DIR="${opensslStatic.dev}/include"
+            export PKG_CONFIG_PATH="${opensslStatic.dev}/lib/pkgconfig"
+            export PS1="(rust-musl-openssl) $PS1"
+          '';
+        };
+      }
+    );
 }
-
--- src/data_models/mod.rs
+++ src/data_models/mod.rs
@@ -2,3 +2,4 @@ pub mod generic_data;
 pub mod instrument_data;
 pub mod hist_data;
 pub mod tick_data;
+pub mod futures_data;
