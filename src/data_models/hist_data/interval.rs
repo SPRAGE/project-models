21bb19a7379dc742f6d38103b6ec519f75c1118b
SPRAGE <shauna.pai@gmail.com>
2025-04-18 23:17:42 +0530

    Add: Implement from_str and from_integer methods for Interval enum

--- src/data_models/hist_data/interval.rs
+++ src/data_models/hist_data/interval.rs
@@ -12,3 +12,31 @@ pub enum Interval {
     SixtyMinute = 7,
 }
 
+impl Interval {
+    pub fn from_str(s: &str) -> Option<Self> {
+        match s {
+            "Day" => Some(Interval::Day),
+            "Minute" => Some(Interval::Minute),
+            "ThreeMinute" => Some(Interval::ThreeMinute),
+            "FiveMinute" => Some(Interval::FiveMinute),
+            "TenMinute" => Some(Interval::TenMinute),
+            "FifteenMinute" => Some(Interval::FifteenMinute),
+            "ThirtyMinute" => Some(Interval::ThirtyMinute),
+            "SixtyMinute" => Some(Interval::SixtyMinute),
+            _ => None,
+        }
+    }
+    pub fn from_integer(i: i8) -> Option<Self> {
+        match i {
+            0 => Some(Interval::Day),
+            1 => Some(Interval::Minute),
+            2 => Some(Interval::ThreeMinute),
+            3 => Some(Interval::FiveMinute),
+            4 => Some(Interval::TenMinute),
+            5 => Some(Interval::FifteenMinute),
+            6 => Some(Interval::ThirtyMinute),
+            7 => Some(Interval::SixtyMinute),
+            _ => None,
+        }
+    }
+}
