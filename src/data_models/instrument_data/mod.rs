04d2d8c9a47639f89822fffad9680b46ee783f22
SPRAGE <shauna.pai@gmail.com>
2025-04-18 17:20:13 +0530

    Feat: Instrument Table creation query

--- src/data_models/instrument_data/mod.rs
+++ src/data_models/instrument_data/mod.rs
@@ -3,12 +3,10 @@ mod exchange;
 mod instrument_type;
 mod segment;
 mod base_exchange;
-
+pub mod queries;
 
 pub use instrument::*;
 pub use exchange::*;
 pub use instrument_type::*;
 pub use segment::*;
 pub use base_exchange::*;
-
-
