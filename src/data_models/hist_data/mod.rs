3c7c748b632e69ba0ba55118d833112dc4404db8
SPRAGE <shauna.pai@gmail.com>
2025-04-18 22:16:55 +0530

    Add: Implement CREATE_HIST_DATA_TABLE query and update module structure

--- src/data_models/hist_data/mod.rs
+++ src/data_models/hist_data/mod.rs
@@ -1,5 +1,6 @@
 mod interval;
 mod hist_data_mod;
+pub mod queries;
 
 pub use interval::*;
 pub use hist_data_mod::*;
---
+++ src/data_models/hist_data/queries/create_table.rs
@@ -0,0 +1,25 @@
+pub static CREATE_HIST_DATA_TABLE:&str = r#"
+CREATE TABLE IF NOT EXISTS hist_data
+(
+    tradingsymbol String,
+    open Float64,
+    high Float64,
+    low Float64,
+    close Float64,
+    volume UInt64,
+    datetime DateTime('Asia/Kolkata'),
+    interval Enum8(
+        'Day' = 0,
+        'Minute' = 1,
+        'ThreeMinute' = 2,
+        'FiveMinute' = 3,
+        'TenMinute' = 4,
+        'FifteenMinute' = 5,
+        'ThirtyMinute' = 6,
+        'SixtyMinute' = 7
+    )
+)
+ENGINE = ReplacingMergeTree
+PARTITION BY (tradingsymbol, interval)
+ORDER BY (tradingsymbol, interval, datetime)
+"#;
